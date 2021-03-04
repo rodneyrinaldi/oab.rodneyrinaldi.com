802270f35001a312d20c9dc51e925347c054f935
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-03-04 16:56:15 -0300

    feat: update home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps /*, AppContext */ } from 'next/app'
 
-import '../styles/globals.css'
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
